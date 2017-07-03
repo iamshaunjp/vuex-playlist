84fbbdb58b30a4157f51047c9f6c274c188641be
iamshaunjp <shaunjp@shaun-pelling.com>
2017-07-03 17:29:12 +0100

    added lesson 8 code

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -12,20 +12,22 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex';
+
 export default {
     computed: {
         products(){
             return this.$store.state.products;
         },
-        saleProducts(){
-            return this.$store.getters.saleProducts;
-        }
+        ...mapGetters([
+            'saleProducts'
+        ])
     },
     methods: {
-        reducePrice: function(amount){
-            // this.$store.commit('reducePrice', amount);
-            this.$store.dispatch('reducePrice', amount);
-        }
+        ...mapActions([
+            'reducePrice'
+        ])
     }
 }
 </script>
