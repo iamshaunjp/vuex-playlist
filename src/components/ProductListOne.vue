b4d88c6e142bb6ce21da4cdd3c0d391d881b25b5
Marko_TON <markotoncanovic@gmail.com>
2022-08-07 13:31:48 +0200

    added :key

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,7 +2,7 @@
     <div id="product-list-one">
         <h2>Product List One</h2>
         <ul>
-            <li v-for="product in saleProducts">
+            <li v-for="product in saleProducts" :key="product.price + 'prodListOne'">
                 <span class="name">{{ product.name }}</span>
                 <span class="price">£{{ product.price }}</span>
             </li>
--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
     <div id="product-list-two">
         <h2>Product List Two</h2>
         <ul>
-            <li v-for="product in saleProducts">
+            <li v-for="product in saleProducts" :key="product.price + 'prodListTwo'">
                 <span class="name">{{ product.name }}</span>
                 <span class="price">£{{ product.price }}</span>
             </li>
