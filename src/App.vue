309bc6ac47129e0876b17baec5c23088702c2324
markoton <markotoncanovic@gmail.com>
2023-06-30 13:27:10 +0200

    npm install and format

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
-      <product-list-one></product-list-one>
-      <product-list-two></product-list-two>
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
 
 <script>
-import ProductListOne from './components/ProductListOne.vue'
-import ProductListTwo from './components/ProductListTwo.vue'
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
 export default {
-    name: 'app',
-    components: {
-        'product-list-one': ProductListOne,
-        'product-list-two': ProductListTwo
-    }
-}
+  name: "app",
+  components: {
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo,
+  },
+};
 </script>
 
 <style>
-body{
-    font-family: Ubuntu;
-    color: #555;
+body {
+  font-family: Ubuntu;
+  color: #555;
 }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -4,38 +4,38 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    strict: true,
-    state: {
-        products: [
-            {name: 'Banana Skin', price: 20},
-            {name: 'Shiny Star', price: 40},
-            {name: 'Green Shells', price: 60},
-            {name: 'Red Shells', price: 80}
-        ]
-    },
-    getters: {
-        saleProducts: (state) => {
-            var saleProducts = state.products.map( product => {
-                return {
-                    name:  '**' + product.name + '**',
-                    price: product.price / 2,
-                };
-            });
-            return saleProducts;
-        }
-    },
-    mutations: {
-        reducePrice: (state, payload) => {
-            state.products.forEach( product => {
-                product.price -= payload
-            });
-        }
-    },
-    actions: {
-        reducePrice: (context, payload) => {
-            setTimeout(function(){ // reach out for data
-                context.commit('reducePrice', payload);
-            }, 2000);
-        }
+  strict: true,
+  state: {
+    products: [
+      { name: 'Banana Skin', price: 20 },
+      { name: 'Shiny Star', price: 40 },
+      { name: 'Green Shells', price: 60 },
+      { name: 'Red Shells', price: 80 }
+    ]
+  },
+  getters: {
+    saleProducts: (state) => {
+      var saleProducts = state.products.map(product => {
+        return {
+          name: '*get->*' + product.name + '**',
+          price: product.price / 2,
+        };
+      });
+      return saleProducts;
     }
+  },
+  mutations: {
+    reducePrice: (state, payload) => {
+      state.products.forEach(product => {
+        product.price -= payload
+      });
+    }
+  },
+  actions: {
+    reducePrice: (context, payload) => {
+      setTimeout(function () { // reach out for data
+        context.commit('reducePrice', payload);
+      }, 2000);
+    }
+  }
 });
